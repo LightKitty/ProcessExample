d873ff440e589296afbc160f283ac501d0e9e62a
wnmiw <383929371@qq.com>
2019-10-10 19:10:35 +0800

    first commit

---
+++ ProcessExample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ProcessExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.process1 = new System.Diagnostics.Process();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(625, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(73, 262);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "启动记事本";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(288, 262);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "停止记事本";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(499, 262);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(75, 23);
+            this.buttonView.TabIndex = 3;
+            this.buttonView.Text = "观察所有进程";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 297);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ProcessExample/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = "notepad.exe";
+            //启动Notepad.exe 进程.
+            process1.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //创建新的Process 组件的数组,并将它们与指定的进程名称（Notepad）的所有进程资源相关联.
+            Process[] myprocesses;
+            myprocesses = Process.GetProcessesByName("Notepad");
+            foreach (Process instance in myprocesses)
+            {
+                //设置终止当前线程前等待1000 毫秒
+                instance.WaitForExit(1000);
+                instance.CloseMainWindow();
+            }
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            //创建Process 类型的数组,并将它们与系统内所有进程相关联
+            Process[] processes;
+            processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                //Idle 指显示CPU 空闲率的进程名称
+                //由于访问Idle 的StartTime 会出现异常，所以将其排除在外
+                if (p.ProcessName != "Idle")
+                {
+                    //将每个进程名和进程开始时间加入listBox1 中
+                    this.listBox1.Items.Add(
+                    string.Format("{0,-30}{1:h:m:s}", p.ProcessName, p.StartTime));
+                }
+            }
+        }
+    }
+}
+
